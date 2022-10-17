b068d2e26d3d7f116bf9f2c5899540aacbbf859e
sammysys <109607439+sammysys@users.noreply.github.com>
2022-10-17 10:01:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,123 +3,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+
+const homeStartingContent = "daily posts about life event any thing special"
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
-
-const articleSchema = {
+const postSchema = {
   title: String,
   content: String
 };
 
-const Article = mongoose.model("Article", articleSchema);
+const Post = mongoose.model("Post", postSchema);
 
-///////////////////////////////////Requests Targetting all Articles////////////////////////
-
-app.route("/articles")
-
-.get(function(req, res){
-  Article.find(function(err, foundArticles){
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-})
-
-.post(function(req, res){
-
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
   });
 
-  newArticle.save(function(err){
-    if (!err){
-      res.send("Successfully added a new article.");
-    } else {
-      res.send(err);
-    }
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
   });
-})
-
-.delete(function(req, res){
-
-  Article.deleteMany(function(err){
-    if (!err){
-      res.send("Successfully deleted all articles.");
-    } else {
-      res.send(err);
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
     }
   });
 });
 
-////////////////////////////////Requests Targetting A Specific Article////////////////////////
+app.get("/posts/:postId", function(req, res){
 
-app.route("/articles/:articleTitle")
+const requestedPostId = req.params.postId;
 
-.get(function(req, res){
-
-  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
-    if (foundArticle) {
-      res.send(foundArticle);
-    } else {
-      res.send("No articles matching that title was found.");
-    }
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
-})
 
-.put(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {title: req.body.title, content: req.body.content},
-    {overwrite: true},
-    function(err){
-      if(!err){
-        res.send("Successfully updated the selected article.");
-      }
-    }
-  );
-})
-
-.patch(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {$set: req.body},
-    function(err){
-      if(!err){
-        res.send("Successfully updated article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
-})
-
-.delete(function(req, res){
-
-  Article.deleteOne(
-    {title: req.params.articleTitle},
-    function(err){
-      if (!err){
-        res.send("Successfully deleted the corresponding article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
 });
 
 
